794908cbe4754562b98abe133cb951ca3c407e46
unknown <amirhasanpezhman@gmail.com>
2025-07-26 08:31:18 +0330

    not done

--- portfolio-website/components/Navbar.tsx
+++ portfolio-website/components/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="bg-gray-800/80 backdrop-blur-sm sticky top-0 z-50 ">
+        <nav className="bg-gray-800/80  backdrop-blur-sm sticky top-0 z-50 ">
             <div className="container mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
